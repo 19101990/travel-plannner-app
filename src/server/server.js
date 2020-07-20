5b50658eda5e1a259f97e8ec46190e2681358e11
19101990 <e19101990@gmail.com>
2020-07-20 16:45:07 +0200

    add require statements to server/server.js

---
+++ src/server/server.js
@@ -0,0 +1,6 @@
+const dotenv = require('dotenv')
+dotenv.config()
+var path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
