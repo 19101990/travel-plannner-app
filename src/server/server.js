60987837fd58dcc35bdf86ca3adb7b9b6be0b16a
19101990 <e19101990@gmail.com>
2020-07-20 16:56:25 +0200

    [bug] fix typo in server/server.js

--- src/server/server.js
+++ src/server/server.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(bodyParser.json)
-app.use(bodyParser.url-encoded({
+app.use(bodyParser.urlencoded({
     extended: true
 }))
 
