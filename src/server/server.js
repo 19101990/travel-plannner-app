0c9baad9056a8dc9e9723591b6fafc9db9eff5f2
19101990 <e19101990@gmail.com>
2020-07-20 16:49:25 +0200

    set up Express.js

--- src/server/server.js
+++ src/server/server.js
@@ -5,3 +5,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+const app = express()
+app.use(cors())
+app.use(bodyParser.json)
+app.use(bodyParser.url-encoded({
+    extended: true
+}))
+
+app.listen(8000, function () {
+    console.log('Example app listening on port 8000!')
+})
