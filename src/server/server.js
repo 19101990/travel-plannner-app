f4ed2455abd501b81f42cde816a7289271442961
19101990 <e19101990@gmail.com>
2020-07-21 10:31:50 +0200

    [bug] update server.js
    server wasn't able to display index.html properly

--- src/server/server.js
+++ src/server/server.js
@@ -1,23 +1,60 @@
-const dotenv = require('dotenv')
-dotenv.config()
 var path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
-app.use(cors())
-app.use(bodyParser.json)
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
 
-app.use(express.static('dist'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use(cors())
+
+
+app.use(express.static('src/client'))
+
+console.log(__dirname)
 
 app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
+    res.sendFile('/client/views/index.html', { root: __dirname + '/..' })
 })
 
-app.listen(8000, function () {
-    console.log('Example app listening on port 8000!')
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!')
 })
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
+
+
+// const dotenv = require('dotenv')
+// dotenv.config()
+// var path = require('path')
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const cors = require('cors')
+
+// const app = express()
+// app.use(cors())
+// app.use(bodyParser.json)
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }))
+
+// app.use(express.static('src/client'))
+
+// console.log(__dirname)
+
+// app.get('/', function (req, res) {
+//     res.sendFile('/client/views/index.html', { root: __dirname + '/..' })
+// })
+// // app.use(express.static('dist'))
+// // console.log(__dirname)
+
+// // app.get('/', function (req, res) {
+// //     res.sendFile(path.join(__dirname + '/../../dist/index.html'))
+// // })
+
+// app.listen(8080, function () {
+//     console.log('Example app listening on port 8080!')
+// })
